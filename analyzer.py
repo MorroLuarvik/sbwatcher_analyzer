c40a9f5c793e4a0273bf9b26da771887d6f0219d
Morro Luarvik <dr.morro.l@gmail.com>
2020-10-30 17:27:12 +1000

    get month mins

--- analyzer.py
+++ analyzer.py
@@ -2,6 +2,11 @@
 #-*-coding:utf-8-*-
 """ Модуль подготовки данных """
 
+DAY_LENGTH = 24 * 3600
+WEEK_LENGTH = 7 * DAY_LENGTH
+TWO_WEEK_LENGTH = 14 * DAY_LENGTH
+MONTH_WEEK_LENGTH = 30 * DAY_LENGTH
+
 import datetime
 
 class Analyzer:
@@ -42,10 +47,42 @@ class Analyzer:
 
 		return rate_range + [rate_range[-1]] + [rate_range[0]], spred_data
 
-	def _get_from_index(self, date_time):
-		""" получить индекс первого элемента большего чем date_time """
-		return None
+	def get_week_sell_mins(self):
+		ret_dates = []
+		ret_vals = []
 
-	def _get_to_index(self, date_time):
-		""" получить индекс последнего элемента меньшего чем date_time """
-		return None
+		first_idx = self._get_left_index(self.raw_info[0]['event_ts'] + MONTH_WEEK_LENGTH)
+
+		for cur_idx, item in enumerate(self.raw_info[first_idx:]):
+			global_start_idx = self._get_left_index(self.raw_info[cur_idx]['event_ts'] - MONTH_WEEK_LENGTH)
+			global_end_idx = cur_idx + first_idx
+
+			range_min = min( [item['sell_price'] for item in self.raw_info[global_start_idx:global_end_idx]])
+			if item['sell_price'] <= range_min:
+				ret_dates.append( datetime.datetime.fromtimestamp(item['event_ts']) )
+				ret_vals.append(item['sell_price'])
+
+		return ret_dates, ret_vals
+
+
+	def _get_left_index(self, date_time):
+		""" получить индекс первого элемента большего чем date_time """
+		left_idx = 0
+		right_idx = len(self.raw_info) - 1
+		
+		if (self.raw_info[right_idx]['event_ts'] < date_time):
+			return right_idx
+
+		if (self.raw_info[left_idx]['event_ts'] > date_time):
+			return left_idx
+
+		if (date_time < self.raw_info[left_idx]['event_ts'] or date_time > self.raw_info[right_idx]['event_ts']):
+			raise Exception('Запрашиваемая дата не в диапазоне')
+		
+		while right_idx - left_idx > 1:
+			mid_idx = (left_idx + right_idx) // 2
+			if (date_time > self.raw_info[mid_idx]['event_ts']):
+				left_idx = mid_idx
+			else:
+				right_idx = mid_idx
+		return left_idx
--- run.py
+++ run.py
@@ -7,21 +7,62 @@ CONFIG_FOLDER_NAME = 'configs'
 
 import os
 import json
-import datetime
+import datetime, time
+
+from mysql import connector as connector
 
 import matplotlib.pyplot as plt
 
-fig1_arr = (
-	[-1, 0, 1, 0, -1],
-	[0, 1, 0, -1, 0]
-)
+dirName, _ = os.path.split(os.path.abspath(''))
+config_file_path = dirName + os.path.sep + os.path.sep + CONFIG_FOLDER_NAME + os.path.sep + CONFIG_FILE_NAME
 
-fig2_arr = (
-	[-1, -1, 1, 1, -1],
-	[-1, 1, 1, -1, -1]
-)
+config_file = open(config_file_path, 'r+')
+configs = json.load(config_file)
+config_file.close()
 
-plt.plot(*fig1_arr)
-plt.plot(*fig2_arr)
+connect = connector.connect(**configs['mysql'])
+
+start_date = datetime.datetime.strptime('2020.1.1', '%Y.%m.%d')
+end_date = datetime.datetime.strptime('2020.10.30', '%Y.%m.%d')
+
+query = """
+SELECT buy_price, sell_price, event_ts 
+FROM f_rates WHERE fin_id = 1 and event_ts BETWEEN {0} AND {1} ORDER BY event_ts;
+""".format(start_date.timestamp(), end_date.timestamp())
+
+cursor = connect.cursor(dictionary=True)
+cursor.execute(query) 
+rates = cursor.fetchall()
+
+from analyzer import Analyzer
+analyz = Analyzer(rates)
+
+from_uts = time.mktime( datetime.datetime.strptime('2020.3.1', '%Y.%m.%d').timetuple() )
+to_uts = time.mktime( datetime.datetime.strptime('2020.3.20', '%Y.%m.%d').timetuple() )
+from_idx = analyz._get_left_index(from_uts)
+to_idx = analyz._get_left_index(to_uts)
+sell_slice = [ item['sell_price'] for item in rates[from_idx:to_idx] ]
+min_sell_val = min(sell_slice)
+min_idx = from_idx + sell_slice.index(min_sell_val)
+print( 'from: {0}, to: {1}: min sell price: {2} at {3}'.format(
+	time.strftime( '%Y.%m.%d %H:%M:%S', time.localtime(from_uts) ), 
+	time.strftime( '%Y.%m.%d %H:%M:%S', time.localtime(to_uts) ), 
+	min_sell_val,
+	time.strftime( '%Y.%m.%d %H:%M:%S', time.localtime(rates[min_idx]['event_ts']) )
+) )
+
+#print( min( [item['sell_price'] for item in rates[from_idx:to_idx]] ) )
+
+#print( analyz.get_week_sell_mins() )
+
+plt.figure(figsize=(18, 6))
+plt.subplots_adjust(left=0.05, right=0.95)
+
+plt.plot( *analyz.get_sells() )
+plt.plot( *analyz.get_buys() )
+
+plt.scatter( *analyz.get_week_sell_mins(), color='green' )
 
 plt.show()
+
+print('finish!')
