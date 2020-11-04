acb9ae804c3de8337a57ff796e5c540228d27924
Morro Luarvik <dr.morro.l@gmail.com>
2020-11-04 20:40:14 +1000

    few mins

--- analyzer.py
+++ analyzer.py
@@ -5,7 +5,43 @@
 DAY_LENGTH = 24 * 3600
 WEEK_LENGTH = 7 * DAY_LENGTH
 TWO_WEEK_LENGTH = 14 * DAY_LENGTH
-MONTH_WEEK_LENGTH = 30 * DAY_LENGTH
+MONTH_LENGTH = 30 * DAY_LENGTH
+QUART_YEAR_LENGTH = 90 * DAY_LENGTH
+HALF_YEAR_LENGTH = 180 * DAY_LENGTH
+YEAR_LENGTH = 365 * DAY_LENGTH
+
+min_values = [
+	{
+		'alias': 'year',
+		'length': YEAR_LENGTH,
+		'datas' : {}
+	},
+	{
+		'alias': 'half_year',
+		'length': HALF_YEAR_LENGTH,
+		'datas' : []
+	},
+	{
+		'alias': 'quart_year',
+		'length': QUART_YEAR_LENGTH,
+		'datas' : []
+	},
+	{
+		'alias': 'month',
+		'length': MONTH_LENGTH,
+		'datas' : []
+	},
+	{
+		'alias': 'two_week',
+		'length': TWO_WEEK_LENGTH,
+		'datas' : []
+	},
+	{
+		'alias': 'week',
+		'length': WEEK_LENGTH,
+		'datas' : []
+	}
+]
 
 import datetime
 
@@ -20,6 +56,7 @@ class Analyzer:
 	def set_data(self, raw_info):
 		""" Установка данных """
 		self.raw_info = raw_info
+		self._set_mins()
 
 	def get_sells(self):
 		""" данные о продажах """
@@ -47,22 +84,50 @@ class Analyzer:
 
 		return rate_range + [rate_range[-1]] + [rate_range[0]], spred_data
 
-	def get_week_sell_mins(self):
-		ret_dates = []
-		ret_vals = []
+	def get_sell_mins(self, idx = 0):
+		""" тестовая функция """
 
-		first_idx = self._get_left_index(self.raw_info[0]['event_ts'] + MONTH_WEEK_LENGTH)
+		week_datas = min_values[idx]['datas'] #self._get_period_mins(MONTH_LENGTH)
+		return [ datetime.datetime.fromtimestamp(uts) for uts in week_datas.keys() ], week_datas.values()
+
+	def _set_mins(self):
+		""" установка всех доступных минимумов """
+		for idx, item in enumerate(min_values):
+			print(idx)
+			cur_mins = self._get_period_mins(item['length'])
+			#copy_mins = cur_mins.copy()
+			for subidx in range(idx):
+				print('subrenga ', subidx)
+				for event_ts in cur_mins.copy():
+					if event_ts in min_values[subidx]['datas']:
+						del(cur_mins[event_ts])
+						#print('delete {0}'.format(event_ts))
+			item['datas'] = cur_mins
+
+		#print(min_values)
+
+
+	def _get_period_mins(self, priod, value_type = 'sell_price'):
+		""" получить словарь минимальных значение типа value_type
+		за период priod секунд  
+		{
+			event_ts1: value1,
+			event_ts2: value2
+		}
+		"""
+		ret = {}
+
+		first_idx = self._get_left_index(self.raw_info[0]['event_ts'] + priod)
 
 		for cur_idx, item in enumerate(self.raw_info[first_idx:]):
-			global_start_idx = self._get_left_index(self.raw_info[cur_idx]['event_ts'] - MONTH_WEEK_LENGTH)
+			global_start_idx = self._get_left_index(self.raw_info[cur_idx]['event_ts'] - priod)
 			global_end_idx = cur_idx + first_idx
 
-			range_min = min( [item['sell_price'] for item in self.raw_info[global_start_idx:global_end_idx]])
-			if item['sell_price'] <= range_min:
-				ret_dates.append( datetime.datetime.fromtimestamp(item['event_ts']) )
-				ret_vals.append(item['sell_price'])
+			range_min = min( [item[value_type] for item in self.raw_info[global_start_idx:global_end_idx]])
+			if item[value_type] <= range_min:
+				ret[ item['event_ts'] ] = item[value_type]
 
-		return ret_dates, ret_vals
+		return ret
 
 
 	def _get_left_index(self, date_time):
--- run.py
+++ run.py
@@ -22,8 +22,8 @@ config_file.close()
 
 connect = connector.connect(**configs['mysql'])
 
-start_date = datetime.datetime.strptime('2020.1.1', '%Y.%m.%d')
-end_date = datetime.datetime.strptime('2020.10.30', '%Y.%m.%d')
+start_date = datetime.datetime.strptime('2018.01.01', '%Y.%m.%d')
+end_date = datetime.datetime.strptime('2019.12.30', '%Y.%m.%d')
 
 query = """
 SELECT buy_price, sell_price, event_ts 
@@ -37,6 +37,8 @@ rates = cursor.fetchall()
 from analyzer import Analyzer
 analyz = Analyzer(rates)
 
+
+"""
 from_uts = time.mktime( datetime.datetime.strptime('2020.3.1', '%Y.%m.%d').timetuple() )
 to_uts = time.mktime( datetime.datetime.strptime('2020.3.20', '%Y.%m.%d').timetuple() )
 from_idx = analyz._get_left_index(from_uts)
@@ -50,10 +52,7 @@ print( 'from: {0}, to: {1}: min sell price: {2} at {3}'.format(
 	min_sell_val,
 	time.strftime( '%Y.%m.%d %H:%M:%S', time.localtime(rates[min_idx]['event_ts']) )
 ) )
-
-#print( min( [item['sell_price'] for item in rates[from_idx:to_idx]] ) )
-
-#print( analyz.get_week_sell_mins() )
+"""
 
 plt.figure(figsize=(18, 6))
 plt.subplots_adjust(left=0.05, right=0.95)
@@ -61,7 +60,17 @@ plt.subplots_adjust(left=0.05, right=0.95)
 plt.plot( *analyz.get_sells() )
 plt.plot( *analyz.get_buys() )
 
-plt.scatter( *analyz.get_week_sell_mins(), color='green' )
+min0 = plt.scatter( *analyz.get_sell_mins(0), marker='x', zorder=10 )
+min1 = plt.scatter( *analyz.get_sell_mins(1), zorder=10 )
+min2 = plt.scatter( *analyz.get_sell_mins(2), zorder=10 )
+min3 = plt.scatter( *analyz.get_sell_mins(3), zorder=10 )
+min4 = plt.scatter( *analyz.get_sell_mins(4), zorder=10 )
+min5 = plt.scatter( *analyz.get_sell_mins(5), zorder=10 )
+
+plt.legend(
+	(min0, min1, min2, min3, min4, min5),
+	('year', 'half year', 'quart year', 'month', 'two week', 'week')
+)
 
 plt.show()
 
